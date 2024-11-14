265fd1069a23b5ee24e6a9c3b0ea108dc5f9a480
jacinta25 <mwendejacinta25@gmail.com>
2024-11-14 10:29:56 +0300

    Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -33,10 +34,6 @@ class Librarian(models.Model):
 
 
 #creating a userprofile
-from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
