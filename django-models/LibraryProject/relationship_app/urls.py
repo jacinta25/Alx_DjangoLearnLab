2aa0e799441f8a08d59b111b1189bf58cc704edc
jacinta25 <mwendejacinta25@gmail.com>
2024-11-17 03:34:01 +0300

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 
+from .views import CustomLoginView, CustomLogoutView, register
+
 urlpatterns = [
+    
     path('books/', list_books, name='list_books'),  # Correct pattern for list_books view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Correct pattern for library_detail view
+
+    path('login/', CustomLoginView.as_view(), name='login'),  # Login view
+    path('logout/', CustomLogoutView.as_view(), name='logout'),  # Logout view
+    path('register/', register, name='register'),  # Registration view
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,10 @@ from .models import Book
 
 from django.views.generic.detail import DetailView
 from .models import Library
+
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def list_books(request):
@@ -15,3 +19,22 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'librariesrelationship_app/library_detail.html'  
     context_object_name = 'library'  
+
+# Login view (Django's built-in view)
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+# Logout view (Django's built-in view)
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+# Registration view
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
