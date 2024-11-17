0748c577e428159968a39c89f42285ef2e885a1d
jacinta25 <mwendejacinta25@gmail.com>
2024-11-17 03:39:23 +0300

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 
-from .views import CustomLoginView, CustomLogoutView, register
+from .views import custom_login, CustomLogoutView, register
 
 urlpatterns = [
     
     path('books/', list_books, name='list_books'),  # Correct pattern for list_books view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Correct pattern for library_detail view
 
-    path('login/', CustomLoginView.as_view(), name='login'),  # Login view
+    path('login/', custom_login, name='login'),  # Custom login view
     path('logout/', CustomLogoutView.as_view(), name='logout'),  # Logout view
     path('register/', register, name='register'),  # Registration view
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,9 +4,10 @@ from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
 
-from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import  redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LogoutView
 # Create your views here.
 
 def list_books(request):
@@ -20,9 +21,17 @@ class LibraryDetailView(DetailView):
     template_name = 'librariesrelationship_app/library_detail.html'  
     context_object_name = 'library'  
 
-# Login view (Django's built-in view)
-class CustomLoginView(LoginView):
-    template_name = 'relationship_app/login.html'
+# Custom login view
+def custom_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)  # This logs the user in
+            return redirect('home')  # Redirect to home page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
 
 # Logout view (Django's built-in view)
 class CustomLogoutView(LogoutView):
