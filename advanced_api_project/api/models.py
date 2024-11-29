5a5a9338bc0e90cf7b4590f69c0f88dd54c7e199
jacinta25 <mwendejacinta25@gmail.com>
2024-11-29 11:50:37 +0300

    Setting Up a New Django Project with Custom Serializers in Django REST Framework

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField(default=2000)
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
