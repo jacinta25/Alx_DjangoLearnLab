c50bd75f7698c37b262669290f04b222b65968c0
jacinta25 <mwendejacinta25@gmail.com>
2024-12-09 15:53:41 +0300

    CRUD OPERATIONS

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -6,6 +6,11 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from .forms import CustomUserCreationForm, UserProfileForm
 
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+
 # Registration view
 def register_view(request):
     if request.method == 'POST':
@@ -32,3 +37,40 @@ def profile_view(request):
         form = UserProfileForm(instance=request.user)
     return render(request, 'blog/profile.html', {'form': form})
 
+
+#Blog post management features
+class PostListView(ListView):
+    model = Post
+    template_name = 'posts/post_list.html'
+    context_object_name = 'posts'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'posts/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'posts/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'posts/post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'posts/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
