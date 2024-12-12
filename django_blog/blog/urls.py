831ba05af60321cc75aa452a253e500ebb679654
jacinta25 <mwendejacinta25@gmail.com>
2024-12-12 19:59:00 +0300

    user authentication

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 from blog.views import SignUpView, profile
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),  # Includes Django auth views (login, logout)
+    path('login/', include('django.contrib.auth.urls')),  # Includes Django auth views (login, logout)
     path('home/', TemplateView.as_view(template_name='blog/home.html'), name='home'),
     path('register/', SignUpView.as_view(), name='register'),
     path('posts/', TemplateView.as_view(template_name='blog/posts.html'), name='posts'),
